7583bdfcab16782d3d8db5c1886fdb341c022612
Habib Shaikh <33100185+habibshaikh14@users.noreply.github.com>
2020-10-23 18:44:53 +0530

    Number wizard mini game.
    - First Commit

---
+++ Number Wizard UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,33 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+    [SerializeField] int min;
+    [SerializeField] int max;
+    [SerializeField] TextMeshProUGUI guessTextObject;
+    int guess;
+
+    void Start()
+    {
+        NextGuess();
+    }
+
+    public void OnPressHigher()
+    {
+        min = (guess + 1 <= max) ? (guess + 1) : guess;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = (guess - 1 >= min) ? (guess - 1) : guess;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = Random.Range(min, max + 1);
+        guessTextObject.text = guess.ToString();
+    }
+}
